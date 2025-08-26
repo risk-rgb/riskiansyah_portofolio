8bccd09ea24e227d153a7fa78b6486cdb27bc2ae
riskiansyah <rriskiansyah10@gmail.com>
2025-08-27 00:20:16 +0700

    Update konfigurasi Tailwind, index.css, dan package untuk build & deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030412',
+        midnight: '#06091f',
+        navy: '#161a31',
+        indigo: '#1f1e39',
+        storm: '#282b4b',
+        aqua: '#33c2cc',
+        mint: '#57db96',
+        royal: '#5c33cc',
+        lavender: '#7a57db',
+        fuchsia: '#ca2f8c',
+        orange: '#cc6033',
+        sand: '#d6995c',
+        coral: '#ea4884',
+      },
+    },
   },
   plugins: [],
 };
